d2ad209cdc1cbb42ba68e61b6f5f6065fd6b6b42
Kennedy Anderson <k_e_nn_e_d_y@hotmail.com>
2022-10-05 09:06:05 -0300

    final curso

--- modules/express.js
+++ modules/express.js
@@ -3,23 +3,43 @@ const UserModel = require("../src/models/user.model");
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+
+//Middleware (função) que executa antes da requisição, precisa chamar o next para liberar a requisição
+app.use((req, res, next) => {
+  console.log(`Request Type: ${req.method}`);
+  console.log(`Content Type: ${req.headers["content-type"]}`);
+  console.log(`Date: ${new Date()}`);
+
+  next();
+});
 
 //app.get("/home", (req, res) => {
 //  res.contentType("application/html");
 //  res.status(200).send("<h1>Hello Word</h1>");
 //});
 
+app.get("/views/users", async (req, res) => {
+  const users = await UserModel.find({});
+
+  res.render("index", { users });
+});
+
+//Seleciona
 app.get("/users", async (req, res) => {
   try {
     const users = await UserModel.find({});
 
-    return res.status(200).json(users);
+    res.status(200).json(users);
   } catch (error) {
     return res.status(500).send(error.message);
   }
 });
 
+//Seleciona por ID
 app.get("/users/:id", async (req, res) => {
   try {
     const id = req.params.id;
@@ -31,13 +51,38 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+//Cria
 app.post("/users", async (req, res) => {
   try {
     const user = await UserModel.create(req.body);
 
-    return res.status(201).json(user);
+    res.status(201).json(user);
   } catch (error) {
     return res.status(500).send(error.message);
+  }
+});
+
+//Update
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+    res.status(201).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+//Delete
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndRemove(id);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 });
 
